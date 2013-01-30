2a30266f91bf59e2ac96da720d52df833d7afc09
YoungZHU <youngzhu321@163.com>
2013-01-30 15:06:43 +0800

    Update Oracle/temp.sql

--- Oracle/temp.sql
+++ Oracle/temp.sql
@@ -6,3 +6,10 @@
 --Format大小写无关。
 --hh24-24小时制
 select to_char(sysdate, 'yyyy-mm-dd hh:mi:ss') from dual;
+
+
+
+-- 多字段的Update语句
+update tbl_a
+set (c_a_1, c_a_2 ... c_a)=(select c_b_1, c_b_2 ... c_b_n from tbl_b where ...)
+where ...
